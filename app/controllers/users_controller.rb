6e8f2199aec7c623bdc38144cd1f1113b5615a0a
komusou.kamen <webengineer.komusou@gmail.com>
2021-02-22 13:34:10 +0000

    user model controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
