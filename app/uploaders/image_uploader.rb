222d41955f09f87bf517791e2271e15ec204af38
komusou.kamen <webengineer.komusou@gmail.com>
2021-03-03 11:00:58 +0000

    add button to navibar image upload validates UI modified

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,6 +44,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   def extension_allowlist
      %w(jpg jpeg)
   end
+  
+  def content_type_allowlist
+    /image\//
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
