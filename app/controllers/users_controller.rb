b1f5323b2c0a0ba09a7e8f897316754774d25a0f
komusou.kamen <webengineer.komusou@gmail.com>
2021-03-04 20:42:34 +0000

    user show rooting and text style change in user and product shows

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   include SessionsHelper
   before_action :require_user_logged_in, only: [:show, :edit, :update, :products]
+  before_action :current_user_page, only: [:show, :edit, :update]
+  
   
   def show
-    @user = @current_user
   end
 
   def new
@@ -23,13 +24,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = @current_user
   end
 
   def update
-    if @current_user.update(user_params)
+    if @user.update(user_params)
       flash[:success] = '正常に更新されました'
-      redirect_to @current_user
+      redirect_to @user
     else
       flash.now[:danger] = '更新に失敗しました'
       render :edit
@@ -37,12 +37,23 @@ class UsersController < ApplicationController
   end
   
   def products
-    @user = @current_user
+    @user = current_user
     @products = current_user.products.page(params[:page])
     #counts(@user)
   end
   
   private
+  
+  def current_user_page
+    @user = User.find_by(id: params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+  
+  #def current_user_page
+  #  if current_user.id != params[:id].to_i
+  #     redirect_to root_url
+  #  end
+  #end
 
   def user_params
     params.require(:user).permit(:name, :email, :phone, :address, :password, :password_confirmation, :image, :introduce)
